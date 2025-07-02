bcd465a364237805cde5dc408818d4271bc7b0ed
ab-progress-tracker <agambajwa270@gmail.com>
2025-07-02 14:34:03 -0400

    Update blockconvolve script---kernel dimensions
    Update allows kernel dimensions to be quickly updated.

--- blockconvolve.py
+++ blockconvolve.py
@@ -5,22 +5,26 @@ import matplotlib.pyplot as plt
 # load image
 array = np.load('array.npy')
 
-kernel = np.array([[1/16, 1/16, 1/16, 1/16], 
-                   [1/16, 1/16, 1/16, 1/16],
-                   [1/16, 1/16, 1/16, 1/16],
-                   [1/16, 1/16, 1/16, 1/16]])
+rows, cols = array.shape
 
-output = np.zeros((8, 8))
+x = 4
+y = 2
+
+x_dim = int(rows/x)
+y_dim = int(cols/y)
+
+kernel = np.full((x,y), 1/(x*y))
+
+output = np.zeros((x_dim, y_dim))
 
 # block convultion; image is 32x32, kernel is 4x4 and moves over 4 px every time to ensure no overlap
 
-for i in range(0, 32, 4):
-    for j in range(0, 32, 4):
-        region = array[i:i+4, j:j+4] # slices a 4x4 section of the the array and convolves it with the kernel
-        if len(kernel) == len(region):
-            multiply = np.dot(region, kernel)
-            pooledvalue = np.sum(multiply)/4
-            output[i//4, j//4] = pooledvalue
+for i in range(0, rows, x):
+    for j in range(0, cols, y):
+        region = array[i:i+x, j:j+y] # slices a 4x4 section of the the array and convolves it with the kernel
+        if kernel.shape == region.shape:
+            pooledvalue = np.sum(region*kernel)
+            output[i//(x), j//(y)] = pooledvalue
 
 print(output) # for debugging
 
